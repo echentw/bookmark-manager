ad4706aacfa41becce39bbaf957f497f71406c0c
Eric Chen <ericchen098@gmail.com>
2019-06-17 00:50:15 -0600

    background image

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,19 @@ module.exports = {
           'sass-loader' // compiles Sass to CSS, using Node Sass by default
         ]
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
     ]
   },
   resolve: {
