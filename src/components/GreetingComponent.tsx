c9d40be8899e34f05cac15e29909cf03922741a4
Eric Chen <ericchen098@gmail.com>
2019-06-18 23:12:09 -0700

    more greeting stuff

--- src/components/GreetingComponent.tsx
+++ src/components/GreetingComponent.tsx
@@ -9,9 +9,12 @@ export class GreetingComponent extends React.Component<Props> {
     return (
       <div className="greeting">
         <div className="greeting-top">
-          Hello, {this.props.name}.
+          <p>Hello, {this.props.name}.</p>
+          <p>The time is 9:43pm.</p>
         </div>
-        <div className="greeting-bottom"/>
+        <div className="greeting-bottom">
+          <p>Tuesday, June 18, 2019.</p>
+        </div>
       </div>
     );
   }
