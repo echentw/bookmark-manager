c0bfd7cc7d18d0fe4293cef2b4aafc474340df72
Eric Chen <ericchen098@gmail.com>
2019-06-22 22:06:04 -0600

    helpers to save and load bookmarks from chrome storage

--- src/ChromeHelpers.ts
+++ src/ChromeHelpers.ts
@@ -6,7 +6,19 @@ export interface TabInfo {
   faviconUrl?: string;
 }
 
+export interface BookmarkData {
+  id: string;
+  url: string;
+  faviconUrl: string;
+  title: string;
+  name: string;
+}
+
 export class ChromeHelpers {
+
+  public static Keys = {
+    Bookmarks: 'bookmarks',
+  };
 
   public static getTabInfos = (): Promise<TabInfo[]> => {
     return new Promise((resolve, reject) => {
@@ -21,8 +33,31 @@ export class ChromeHelpers {
     });
   }
 
-  public static saveBookmarks = (bookmarks: Bookmark[]): void => {
-    // TODO: implement me!
+  public static saveBookmarks = (bookmarks: Bookmark[]): Promise<{}> => {
+    const data: BookmarkData[] = bookmarks.map(bookmark => ({
+      id: bookmark.id,
+      url: bookmark.url,
+      faviconUrl: bookmark.faviconUrl,
+      title: bookmark.title,
+      name: bookmark.name,
+    }));
+
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.set({ [ChromeHelpers.Keys.Bookmarks]: data }, () => {
+        // TODO: error handling
+        resolve();
+      });
+    });
   }
 
+  public static loadBookmarks = (): Promise<Bookmark[]> => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get([ChromeHelpers.Keys.Bookmarks], (result) => {
+        const datas: BookmarkData[] = result[ChromeHelpers.Keys.Bookmarks];
+        const bookmarks: Bookmark[] = datas.map(data => new Bookmark(data));
+        // TODO: error handling
+        resolve(bookmarks);
+      });
+    });
+  }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,3 @@ import * as ReactDOM from 'react-dom';
 import { AppComponent } from './components/AppComponent';
 
 ReactDOM.render(<AppComponent/>, document.getElementById('main'));
-
-
-
-import { ChromeHelpers } from './ChromeHelpers';
-
-const testFunc = async () => {
-  const tabInfos = await ChromeHelpers.getTabInfos();
-  console.log(tabInfos);
-};
-
-testFunc();
