a167de34b3e5750f799a9514efaed14d8544a557
Eric Chen <ericchen098@gmail.com>
2019-07-15 12:56:05 -0500

    move buttons into separate component

---
+++ src/components/BookmarkButtonsComponent.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { IconContext } from 'react-icons';
+import { FaPen, FaCopy, FaTrash } from 'react-icons/fa';
+import * as CopyToClipboard from 'react-copy-to-clipboard';
+import { connect } from 'react-redux';
+
+import * as CopyUrlActions from '../actions/CopyUrlActions';
+import * as EditBookmarkActions from '../actions/EditBookmarkActions';
+import { EditBookmarkParams } from '../actions/EditBookmarkActions';
+
+import { Bookmark } from '../Bookmark';
+import { ShowToastParams } from '../actions/CopyUrlActions';
+import { AppState } from './AppComponent';
+
+interface ExternalProps {
+  bookmark: Bookmark,
+}
+
+interface InternalProps extends ExternalProps {
+  showCopyUrlToast: (params: ShowToastParams) => void;
+  beginEdit: (params: EditBookmarkParams) => void;
+  deleteBookmark: (params: EditBookmarkParams) => void;
+}
+
+class BookmarkButtonsComponent extends React.Component<InternalProps> {
+
+  onClickDelete = (event: React.MouseEvent<SVGElement, MouseEvent>) => {
+    this.props.deleteBookmark({ bookmark: this.props.bookmark });
+  }
+
+  onClickCopy = (event: React.MouseEvent<SVGElement, MouseEvent>) => {
+    const { clientX, clientY } = event;
+    this.props.showCopyUrlToast({ x: clientX, y: clientY });
+  }
+
+  onClickEdit = () => {
+    this.props.beginEdit({ bookmark: this.props.bookmark });
+  }
+
+  render() {
+    return (
+      <IconContext.Provider value={{ size: '1.2em' }}>
+        <CopyToClipboard text={this.props.bookmark.url}>
+          <FaCopy className="bookmark-icon" onClick={this.onClickCopy}/>
+        </CopyToClipboard>
+        <FaPen className="bookmark-icon" onClick={this.onClickEdit}/>
+        <FaTrash className="bookmark-icon" onClick={this.onClickDelete}/>
+      </IconContext.Provider>
+    );
+  }
+}
+
+const mapStateToProps = (state: AppState, props: ExternalProps) => {
+  return {};
+};
+
+const mapActionsToProps = {
+  showCopyUrlToast: CopyUrlActions.showToast,
+  beginEdit: EditBookmarkActions.beginEdit,
+  deleteBookmark: EditBookmarkActions.deleteBookmark,
+};
+
+const Component = connect(mapStateToProps, mapActionsToProps)(BookmarkButtonsComponent);
+export { Component as BookmarkButtonsComponent };
--- src/components/BookmarkComponent.tsx
+++ src/components/BookmarkComponent.tsx
@@ -1,64 +1,29 @@
 import * as React from 'react';
-import { IconContext } from 'react-icons';
-import { FaPen, FaCopy, FaTrash } from 'react-icons/fa';
-import * as CopyToClipboard from 'react-copy-to-clipboard';
-import { connect } from 'react-redux';
-
-import * as CopyUrlActions from '../actions/CopyUrlActions';
-import * as EditBookmarkActions from '../actions/EditBookmarkActions';
-import { ShowToastParams } from '../actions/CopyUrlActions';
-import { EditBookmarkParams } from '../actions/EditBookmarkActions';
-import { AppState } from './AppComponent';
 
 import { EditBookmarkComponent } from './EditBookmarkComponent';
+import { BookmarkButtonsComponent } from './BookmarkButtonsComponent';
 import { Bookmark } from '../Bookmark';
 
-interface ExternalProps {
+interface Props {
   bookmark: Bookmark;
   editing: boolean;
   isDragging: boolean;
 }
 
-interface InternalProps extends ExternalProps {
-  showCopyUrlToast: (params: ShowToastParams) => void;
-  beginEdit: (params: EditBookmarkParams) => void;
-  deleteBookmark: (params: EditBookmarkParams) => void;
-}
-
-class BookmarkComponent extends React.Component<InternalProps> {
-
-  onClickDelete = (event: React.MouseEvent<SVGElement, MouseEvent>) => {
-    this.props.deleteBookmark({ bookmark: this.props.bookmark });
-  }
-
-  onClickCopy = (event: React.MouseEvent<SVGElement, MouseEvent>) => {
-    const { clientX, clientY } = event;
-    this.props.showCopyUrlToast({ x: clientX, y: clientY });
-  }
-
-  onClickEdit = () => {
-    this.props.beginEdit({ bookmark: this.props.bookmark });
-  }
-
+export class BookmarkComponent extends React.Component<Props> {
   render() {
     const { editing, isDragging, bookmark } = this.props;
 
-    const bookmarkNameElement = editing ? (
+    const bookmarkName = editing ? (
       <EditBookmarkComponent bookmark={bookmark}/>
     ) : (
       <a className="bookmark-text" href={bookmark.url}>{bookmark.displayName()}</a>
     );
 
-    const buttonElements = editing ? (
+    const maybeButtons = editing ? (
       null
     ) : (
-      <IconContext.Provider value={{ size: '1.3em' }}>
-        <CopyToClipboard text={bookmark.url}>
-          <FaCopy className="bookmark-icon" onClick={this.onClickCopy}/>
-        </CopyToClipboard>
-        <FaPen className="bookmark-icon" onClick={this.onClickEdit}/>
-        <FaTrash className="bookmark-icon" onClick={this.onClickDelete}/>
-      </IconContext.Provider>
+      <BookmarkButtonsComponent bookmark={bookmark}/>
     );
 
     const classes = isDragging ? 'bookmark dragging' : 'bookmark';
@@ -66,22 +31,9 @@ class BookmarkComponent extends React.Component<InternalProps> {
     return (
       <div className={classes}>
         <img className="bookmark-favicon" src={bookmark.faviconUrl}/>
-        { bookmarkNameElement }
-        { buttonElements }
+        { bookmarkName }
+        { maybeButtons }
       </div>
     );
   }
 }
-
-const mapStateToProps = (state: AppState, props: ExternalProps) => {
-  return {};
-};
-
-const mapActionsToProps = {
-  showCopyUrlToast: CopyUrlActions.showToast,
-  beginEdit: EditBookmarkActions.beginEdit,
-  deleteBookmark: EditBookmarkActions.deleteBookmark,
-};
-
-const Component = connect(mapStateToProps, mapActionsToProps)(BookmarkComponent);
-export { Component as BookmarkComponent };
