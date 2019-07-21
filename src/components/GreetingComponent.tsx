ba48f8da567a748a759f56113f2a1e1230b804ad
Eric Chen <ericchen098@gmail.com>
2019-07-21 13:07:45 -0500

    don't pad hours

--- src/components/GreetingComponent.tsx
+++ src/components/GreetingComponent.tsx
@@ -48,7 +48,7 @@ export class GreetingComponent extends React.Component<Props, State> {
     const hoursNumber = date.getHours() <= 12 ? date.getHours() : date.getHours() - 12;
     const minutesNumber = date.getMinutes();
 
-    const hours = hoursNumber < 10 ? `0${hoursNumber}` : `${hoursNumber}`;
+    const hours = hoursNumber === 0 ? '12' : `${hoursNumber}`;
     const minutes = minutesNumber < 10 ? `0${minutesNumber}` : `${minutesNumber}`;
     const period = date.getHours() < 12 ? 'am' : 'pm';
 
