e61e7297b34ae0c451dfe055d79929ad59d2c681
Eric Chen <ericchen098@gmail.com>
2019-08-18 12:32:13 -0500

    good morning/afternoon/evening

--- src/components/GreetingComponent.tsx
+++ src/components/GreetingComponent.tsx
@@ -18,12 +18,24 @@ export class GreetingComponent extends React.Component<Props> {
     return `${hours}:${minutes} ${period}`;
   }
 
+  dateToPeriod = (date: Date): string => {
+    const hour = date.getHours();
+    if (hour >= 5 && hour <= 11) {
+      return 'morning';
+    } else if (hour >= 12 && hour <= 17) {
+      return 'afternoon';
+    } else {
+      return 'evening';
+    }
+  }
+
   render() {
     const time = this.dateToTime(this.props.date);
+    const period = this.dateToPeriod(this.props.date);
     return (
       <div className="greeting">
         <div className="time-text">{time}</div>
-        <div className="name-text">Good evening, {this.props.name}.</div>
+        <div className="name-text">Good {period}, {this.props.name}.</div>
       </div>
     );
   }
