11fae7283269aaaac50159d6217ef61f8629beb1
Eric Chen <ericchen098@gmail.com>
2019-09-27 04:42:51 -0500

    add real favicons

---
+++ src/assets/delete.sh
@@ -0,0 +1,4 @@
+rm axle16x16.png
+rm axle32x32.png
+rm axle48x48.png
+rm axle128x128.png
---
+++ src/assets/generate.sh
@@ -0,0 +1,4 @@
+convert axle.png -resize 16x16 axle16x16.png
+convert axle.png -resize 32x32 axle32x32.png
+convert axle.png -resize 48x48 axle48x48.png
+convert axle.png -resize 128x128 axle128x128.png
