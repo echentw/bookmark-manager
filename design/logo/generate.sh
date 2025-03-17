d4c242d97f8c01febdffb5b05f93952f9730f78d
Eric Chen <ericchen098@gmail.com>
2025-03-16 20:09:32 -0700

    upgrade to chrome manifest v3; remove google analytics

--- design/logo/generate.sh
+++ design/logo/generate.sh
@@ -3,7 +3,7 @@ mkdir -p generated
 rm generated/*
 
 # Generate the Axle logo in all the appropriate sizes.
-convert Axle512x512.png -resize 16x16 generated/Axle16x16.png
-convert Axle512x512.png -resize 32x32 generated/Axle32x32.png
-convert Axle512x512.png -resize 48x48 generated/Axle48x48.png
-convert Axle512x512.png -resize 128x128 generated/Axle128x128.png
+magick Axle512x512.png -resize 16x16 generated/Axle16x16.png
+magick Axle512x512.png -resize 32x32 generated/Axle32x32.png
+magick Axle512x512.png -resize 48x48 generated/Axle48x48.png
+magick Axle512x512.png -resize 128x128 generated/Axle128x128.png
